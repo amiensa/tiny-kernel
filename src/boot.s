aa21257dcd2c69a07be6ab58762cfaf65a0ad45c
amine <amiensa@proton.me>
2026-03-05 23:45:19 +0100

    hello world x86

--- src/boot.s
+++ src/boot.s
@@ -4,22 +4,29 @@ org 0x7c00
 
 _start:
 
-    mov eax, 0
-    mov ebx, eax
-    mov es, eax
-    mov ss, eax
+    mov ah, 0h
+    mov ax, 0h
+    mov al, ah
+    mov bl, ah
+    mov bh, ah
+    mov dh, ah
+    mov dl, ah
+    mov es, ax
+    mov ss, ax
     mov sp, 0x9000
 
-    mov ah, 0Eh
-    mov al, [k]
-    ;mov cx, 12
-    ;mov bp, hello
+
+    mov ah, 13h
+    mov al, 0
+    mov bh, 0
+    mov bl, 4h
+    mov cx, 12
+    mov bp, hello
     int 10h
     jmp loop
 
 
-;hello db "hello world!", 0
-k db 'a'
+hello db "hello world!", 0
 
 loop:
     jmp loop
